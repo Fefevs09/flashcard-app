ad79dcf5b548487dd266f4a3629ebad7ad80bf3a
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-07 19:03:10 -0300

    fix: add SafeAreaView

--- App.jsx
+++ App.jsx
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 import HeaderCard from './components/Header.jsx'
 import Decks from './screens/Decks.jsx'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <HeaderCard name={'Decks'} />
       <Decks />
-    </View>
+    </SafeAreaView>
   )
 }
 
