cdc574037f7945bef6ee9173651119c9cb5471b7
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-06 23:27:57 -0300

    feat: add header to app

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Feather from 'react-native-vector-icons/Feather'
+const HeaderCard = () => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.test}>
+        <Feather name="zap" size={30} color={'#2E82DB'} />
+      </View>
+      <Text style={styles.textHeader}> Cards </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: 'baseline',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 60
+  },
+  textHeader: {
+    fontSize: 32,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  logo: {
+    width: 20,
+    height: 20
+  }
+})
+
+export default HeaderCard
