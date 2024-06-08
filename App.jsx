e54668da9f42637b1ba1f5e03ac52b679a260354
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-08 11:23:24 -0300

    feat: add review screen

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { DeckScreen } from './screens/Decks.jsx'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import { CardScreen } from './screens/Cards.jsx'
+import { ReviewScreen } from './screens/Review.jsx'
 
 const Stack = createNativeStackNavigator()
 
@@ -17,6 +18,18 @@ const MyStack = () => {
         <Stack.Screen
           name="Card"
           component={CardScreen}
+          options={{
+            title: 'Ciência de Dados',
+            headerStyle: { backgroundColor: '#0D243D' },
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 24 },
+            headerBackTitleVisible: false,
+            headerTintColor: '#fff'
+          }}
+        />
+
+        <Stack.Screen
+          name="Review"
+          component={ReviewScreen}
           options={{
             title: 'Ciência de Dados',
             headerStyle: { backgroundColor: '#0D243D' },
---
+++ screens/Review.jsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native'
+
+export const ReviewScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Review Screen</Text>
+    </SafeAreaView>
+  )
+}
