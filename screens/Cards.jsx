cb1aa65ff88b6d3c9c3e859574564c3b6a996996
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-09 12:22:51 -0300

    feat: add model ListCard

--- screens/Cards.jsx
+++ screens/Cards.jsx
@@ -1,51 +1,20 @@
 import { FlatList, SafeAreaView, StyleSheet } from 'react-native'
 import { Card } from '../components/Card'
 import { TextButton } from '../components/Button'
-const cardsList = [
-  {
-    id: 1,
-    question: 'Qual a biblioteca do python é usada?',
-    answer: 'Pandas, numpy e MatplotLib'
-  },
-  {
-    id: 2,
-    question: "Quais são os V's em Big Data?",
-    answer: 'Volume, Velocidade, Variedade, Veracidade e Valor'
-  },
-  {
-    id: 3,
-    question: 'O que é Ciência de Dados?',
-    answer: 'Volume, Velocidade, Variedade, Veracidade e Valor'
-  },
-  {
-    id: 4,
-    question:
-      'Quais são os principais métodos de aprendizado de máquina em Ciência de Dados?',
-    answer: 'Supervisionado, Não Supervisionado, Aprendizado por Reforço'
-  },
-  {
-    id: 5,
-    question:
-      'Quais são algumas ferramentas comuns usadas em Ciência de Dados?',
-    answer: 'Python, R, Tableau, Power BI, SQL, NoSQL (MongoDB, Cassandra)'
-  },
-  {
-    id: 6,
-    question: 'Quais são os componentes principais da Ciência de Dados?',
-    answer:
-      'Coleta de Dados, Processamentos de Dados, Análise de Dados, Visualização de Dados, Tomada de Decisões'
-  }
-]
+import { ListCard } from '../model/ListCard'
 
 export const CardScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={cardsList}
+        data={ListCard}
         renderItem={({ item }) => <Card title={item.question} />}
         keyExtractor={item => item.id}
       />
-      <TextButton title={'Iniciar Revisão'} navigation={navigation} />
+      <TextButton
+        title={'Iniciar Revisão'}
+        onPress={() => navigation.navigate('Review')}
+      />
     </SafeAreaView>
   )
 }
