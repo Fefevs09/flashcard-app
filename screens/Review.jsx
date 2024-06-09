9c1f1cc5c690d5f82e039b324e995eef15273380
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-09 12:23:48 -0300

    feat: add card reveiw interactive

--- screens/Review.jsx
+++ screens/Review.jsx
@@ -2,55 +2,51 @@ import { SafeAreaView, StyleSheet, Text } from 'react-native'
 import { ReviewCard } from '../components/Question'
 import { Feed } from '../components/Feed'
 import { useState } from 'react'
+import { TextButton } from '../components/Button'
+import { ListCard } from '../model/ListCard'
 
 export const ReviewScreen = ({ navigation }) => {
-  const [question, setQuestions] = useState([
-    {
-      id: 1,
-      question: 'Qual a biblioteca do python é usada?',
-      answer: 'Pandas, numpy e MatplotLib'
-    },
-    {
-      id: 2,
-      question: "Quais são os V's em Big Data?",
-      answer: 'Volume, Velocidade, Variedade, Veracidade e Valor'
-    },
-    {
-      id: 3,
-      question: 'O que é Ciência de Dados?',
-      answer: 'Volume, Velocidade, Variedade, Veracidade e Valor'
-    },
-    {
-      id: 4,
-      question:
-        'Quais são os principais métodos de aprendizado de máquina em Ciência de Dados?',
-      answer: 'Supervisionado, Não Supervisionado, Aprendizado por Reforço'
-    },
-    {
-      id: 5,
-      question:
-        'Quais são algumas ferramentas comuns usadas em Ciência de Dados?',
-      answer: 'Python, R, Tableau, Power BI, SQL, NoSQL (MongoDB, Cassandra)'
-    },
-    {
-      id: 6,
-      question: 'Quais são os componentes principais da Ciência de Dados?',
-      answer:
-        'Coleta de Dados, Processamentos de Dados, Análise de Dados, Visualização de Dados, Tomada de Decisões'
-    }
-  ])
-
+  const [index, setIndex] = useState(0)
+  const [card, setCard] = useState(ListCard[0])
+  const [question, setQuestion] = useState(card.question)
+  const [answer, setAnswer] = useState(card.answer)
   const [showAnswer, setShowAnswer] = useState(false)
+  const [finishDeck, setFinishDeck] = useState(false)
+
+  function nextQuestion() {
+    const newIndex = index + 1
+    if (newIndex < ListCard.length) {
+      const newCard = ListCard[newIndex]
+      setIndex(newIndex)
+      setCard(newCard)
+      setQuestion(newCard.question)
+      setAnswer(newCard.answer)
+      setShowAnswer(false) // Reset showAnswer for new question
+    } else {
+      // Handle the end of the list scenario, e.g., reset or show a message
+      console.log('No more questions available.')
+      setFinishDeck(true)
+    }
+  }
+
+  function backDeck() {
+    navigation.pop()
+  }
 
   return (
     <SafeAreaView style={styles.reviewContainer}>
       <ReviewCard
-        questionTitle={question[0].question}
-        answer={question[0].answer}
+        questionTitle={question}
+        answer={answer}
         showAnswer={showAnswer}
         onPress={() => setShowAnswer(!showAnswer)}
       />
       <Feed />
+      {finishDeck ? (
+        <TextButton onPress={backDeck} title={'Voltar'} />
+      ) : (
+        <TextButton onPress={nextQuestion} title={'Próxima'} />
+      )}
     </SafeAreaView>
   )
 }
