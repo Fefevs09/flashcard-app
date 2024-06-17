a8519d7b8528ed4ad67400ebf3bc5f5acc805b32
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-06-17 12:00:23 -0300

    feat: add create card screen

--- App.jsx
+++ App.jsx
@@ -4,7 +4,9 @@ import { NavigationContainer } from '@react-navigation/native'
 import { CardScreen } from './screens/Cards.jsx'
 import { ReviewScreen } from './screens/Review.jsx'
 import { ModalScreen } from './screens/Modal.jsx'
-import { LogBox } from 'react-native'
+import { Button, LogBox } from 'react-native'
+import { CreateCardScreen } from './screens/CreateCard.jsx'
+import Feather from 'react-native-vector-icons/Feather'
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state'
@@ -24,12 +26,6 @@ const MyStack = () => {
         <Stack.Screen
           name="Card"
           component={CardScreen}
-          // options={{
-          //   headerStyle: { backgroundColor: '#0D243D' },
-          //   headerTitleStyle: { fontWeight: 'bold', fontSize: 24 },
-          //   headerBackTitleVisible: false,
-          //   headerTintColor: '#fff'
-          // }}
           options={({ route }) => ({
             title: route.params.title,
             headerStyle: { backgroundColor: '#0D243D' },
@@ -44,6 +40,18 @@ const MyStack = () => {
           component={ReviewScreen}
           options={{
             title: 'Ciência de Dados',
+            headerStyle: { backgroundColor: '#0D243D' },
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 24 },
+            headerBackTitleVisible: false,
+            headerTintColor: '#fff'
+          }}
+        />
+
+        <Stack.Screen
+          name="Create Card"
+          component={CreateCardScreen}
+          options={{
+            title: 'Novo Card',
             headerStyle: { backgroundColor: '#0D243D' },
             headerTitleStyle: { fontWeight: 'bold', fontSize: 24 },
             headerBackTitleVisible: false,
---
+++ screens/CreateCard.jsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native'
+
+export const CreateCardScreen = ({ route, navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Create card screen</Text>
+    </SafeAreaView>
+  )
+}
