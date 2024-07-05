77e80642a119be80ca38c48d5fa7a5afc7f6eb31
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-07-05 18:43:20 -0300

    feat(tab bar): add icons to tab bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,33 @@
 import { Tabs } from 'expo-router'
+import Feather from '@expo/vector-icons/Feather'
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="decks" />
-      <Tabs.Screen name="settings" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#2E82DB'
+      }}
+    >
+      <Tabs.Screen
+        name="decks"
+        options={{
+          title: 'Decks',
+          tabBarIcon: ({ color }) => (
+            <Feather name="layers" size={24} color={color} />
+          ),
+          headerShown: false
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Ajustes',
+          tabBarIcon: ({ color }) => (
+            <Feather name="settings" size={24} color={color} />
+          ),
+          headerShown: false
+        }}
+      />
       <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
   )
